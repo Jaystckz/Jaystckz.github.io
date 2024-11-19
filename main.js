0acc8f18ceebcf30b1d50eb361a82ee2414aaf25
Jaystckz <jamiuabolajilanre@gmail.com>
2024-11-19 17:25:21 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let temperature = 20
+if (temperature >= 20) {
+    console.log ("it is warm")
+}
+
+let grade =  100
+if (grade <= 100) {
+    console.log ("C")
+} else if (grade > 70) {
+    console.log ("a")
+}
